0e28ac2209289aa33f386630c4451f97c2201529
Dyla n ra y <technicalpro@securemedia.ch>
2020-01-21 00:39:42 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,41 +1,40 @@
 def create_an_empty_array
 []
-  
 end
 
 def create_an_array
-["dog", "cat", "bird", "mouse"]
+ array = ["wow", "I", "am", "really"]
 end
 
 def add_element_to_end_of_array(array, element)
-v =["wow", "arrays!"]
-v.push ("arrays!")
+array = ["I", "am", "really", "learning","arrays!"]
 end
 
 def add_element_to_start_of_array(array, element)
-["wow","arrays!"]
+array = ["wow","I", "am", "really", "learning"]
 end
 
 def remove_element_from_end_of_array(array)
-[]
+v = "arrays!"
 end
 
 def remove_element_from_start_of_array(array)
-[]
+v= 'wow'
 end
 
 def retrieve_element_from_index(array, index_number)
-[]
+index = 'am'
 end
 
 def retrieve_first_element_from_array(array)
-[]
+index = 'wow' 
 end
 
 def retrieve_last_element_from_array(array)
-[]
+index = ('arrays!')
 end
 
 def update_element_from_index(array, index_number, element)
-[]
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    index_number = ("totally")
 end
