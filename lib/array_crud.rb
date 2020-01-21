c9183d7a004bce6ef2e7a1f4c35017f96370821e
Dyla n ra y <technicalpro@securemedia.ch>
2020-01-21 00:39:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-array = ["I", "am", "really", "learning","wow"]
+array = ["I", "am", "really", "learning","arrays!"]
 end
 
 def add_element_to_start_of_array(array, element)
-array = ["hello","I", "am", "really", "learning"]
+array = ["wow","I", "am", "really", "learning"]
 end
 
 def remove_element_from_end_of_array(array)
